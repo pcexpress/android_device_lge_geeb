13a65cd972ace929192ea350c21d70eed297cd02
pcexpress <pcexpress805@gmail.com>
2013-09-05 13:08:26 -0700

    Update full_geeb.mk

--- full_geeb.mk
+++ full_geeb.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := full_geeb
+PRODUCT_NAME := slim_geeb
 PRODUCT_DEVICE := geeb
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Optimus G
