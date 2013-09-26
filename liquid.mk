255e3a51bda56ef746d5599ff7fc269b9c8c44d0
pcexpress <pcexpress805@gmail.com>
2013-09-25 17:13:19 -0700

    Create liquid.mk

---
+++ liquid.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Target Release Name
+PRODUCT_RELEASE_NAME := liquid_mako
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Standard
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# GSM RIL Parts
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mako
+PRODUCT_NAME := liquid_mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.mako.$(shell date +%m%d%y).$(shell date +%H%M%S)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=737497 \
+    PRODUCT_NAME=occam \
+    TARGET_DEVICE=occam \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    BUILD_FINGERPRINT="google/occam/mako:4.3/JWR66V/737497:user/release-keys" \
+    PRIVATE_BUILD_DESC="occam-user 4.3 JWR66V 737497 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
